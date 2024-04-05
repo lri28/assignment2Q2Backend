58d6741f192732c20faa4b697503a23234bfc991
lri28 <leonarose.ilao@gmail.com>
2024-04-04 23:59:55 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,10 @@ const controller = require('./controller');
 // Route for registration form submission
 router.post('/register', controller.registerUser);
 
+// Route for rendering the registration page
+router.get('/register', (req, res) => {
+    // Handle the GET request here, such as rendering an HTML page
+    res.sendFile(__dirname + '/registration.html');
+});
+
 module.exports = router;
